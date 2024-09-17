97a0f65e0639554a2ef821bc651eab532d510528
Haimeng Zhao <haimengzhao@icloud.com>
2024-09-18 05:19:28 +0800

    combined plot

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_mean_std_shaded(x, mean, std, color, label, **args):
-    plt.plot(x, mean, color=color, label=label, **args)
-    plt.fill_between(x, mean-std, mean+std, color=color, alpha=0.2)
+def plot_mean_std_shaded(ax, x, mean, std, color, label, **args):
+    ax.plot(x, mean, color=color, label=label, **args)
+    ax.fill_between(x, mean-std, mean+std, color=color, alpha=0.2)
     
 def plot_mean_std_cap(x, mean, std, color, label, **args):
     plt.errorbar(x, mean, yerr=std, fmt='o-', color=color, label=label, capsize=5, **args)
